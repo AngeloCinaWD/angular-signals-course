6252ef91fdc9ca48dde70416790068d0af13531e
unknown <a.cina@procreatech.com>
2025-01-07 20:19:36 +0100

    12. Angular Signal Effects - Setting The Injection Context

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {
+  afterNextRender,
   Component,
   computed,
   effect,
@@ -52,15 +53,34 @@ export class HomeComponent {
 
   values: WritableSignal<number[]> = signal<number[]>([0]);
 
+  // inietto l'Injector
+  injector = inject(Injector);
+
   constructor() {
     // se volessi far fare qualcosa ogni volta che un signal cambia il suo valore, posso dichiarare un PURE SIDE EFFECT
     // utilizzando l'API effect() qui nel costruttore
     // nel corpo della funzione indico quello che devve accadere quando un signal cambia valore, invocandolo
     // posso invocare anche un computed signal
     // non vanno utilizzati spesso i pure side effects è meglio utilizzare le funzioni in modo esplicito, ad esempio salvare i dati di un form in un BE quando clicco un button
-    effect(() => {
-      // console.log('Value of counter signal: ' + this.counterSignal().value);
-      console.log('Value of computed signal: ' + this.tenXCounter());
+    // gli effect possono creare un memory leak se non puliti per bene quando il componente viene distrutto
+    // questo perchè per creare un effect() va invocato un signal e questo vuol dire creare una dependency che deve essere ripulita per non rimanere in memoria
+    // angular distrugge e ripulisce automaticamente la funzione effect() ma la deve trovare, se messa nel costruttore angular la trova
+    // sevolessi utilizzarla in un altro momento, ad esempio dopo aver renderizzato il componente angular non la troverebbe e mi darebbe un errore, posso evitare questo errore utilizzando la funzione inject() (che funziona come se si trattasse del costruttore), iniettare l'oggetto Injector e poi indicarlo nella funzione effect() nel suo oggetto di configurazione
+    // in questo modo angular sa dove andare a trovare la funzione effect(), dove è sata iniettata e distruggerla per evitare leak di memoria
+    // effect(() => {
+    //   // console.log('Value of counter signal: ' + this.counterSignal().value);
+    //   console.log('Value of computed signal: ' + this.tenXCounter());
+    // });
+    afterNextRender(() => {
+      effect(
+        () => {
+          // console.log('Value of counter signal: ' + this.counterSignal().value);
+          console.log('Value of computed signal: ' + this.tenXCounter());
+        },
+        {
+          injector: this.injector,
+        }
+      );
     });
   }
 
