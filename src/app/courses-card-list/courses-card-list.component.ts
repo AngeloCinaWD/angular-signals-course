b8aafabcc0b7a7771634e85b173f964e81bcd608
unknown <a.cina@procreatech.com>
2025-05-08 09:25:46 +0200

    alignment commit

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -11,19 +11,7 @@ import { Transform } from 'stream';
   styleUrl: './courses-card-list.component.scss',
 })
 export class CoursesCardListComponent {
-  // passiamo i corsi da visualizzare tramite signal inputs
-  // rendiamo required, obbligatoria, l'input
-  // un'input obbligatoria non permette di passare un valore iniziale di default
-  //   courses: InputSignal<Course[]> = input.required<Course[]>();
-  // i signals hanno sempre un valore iniziale, quindi nel caso si utilizzi una input required e non si può passare il valore iniziale, angular la considererà come undefined
-  // utilizzo l'input come non obbligatoria e definisco come initial value un array vuoto
-  courses: InputSignal<Course[]> = input<Course[]>([], {
-    // possiamo passare un oggetto con delle opzioni
-    //   alias: cambio il nome con cui deve essere chiamata questa input nel template del parent, ad esempio
-    //   alias: 'courses-alias',
-    //   in questo modo il nome della input da valorizzare non sarà più courses ma courses-alias [course-alias]="valore da passare"
-    //   altra property è trasform, che ci permette di lavorare sull'input che passiamo dal parent
-  });
+  courses: InputSignal<Course[]> = input.required<Course[]>();
 
   constructor() {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -48,10 +48,10 @@ export class HomeComponent implements OnInit {
   });
 
   constructor() {
-    // effect(() => {
-    //   console.log(`Beginner courses: `, this.beginnerCourses());
-    //   console.log(`Advanced courses: `, this.advancedCourses());
-    // });
+    effect(() => {
+      console.log(`Beginner courses: `, this.beginnerCourses());
+      console.log(`Advanced courses: `, this.advancedCourses());
+    });
 
     this.loadCourses().then(() => console.log(this.#courses()));
   }
@@ -61,8 +61,6 @@ export class HomeComponent implements OnInit {
   async loadCourses() {
     try {
       const courses = await this.coursesService.loadAllCourses();
-      // this.#courses.set(courses);
-      // ordiniamo i corsi secondo seqNo
       this.#courses.set(courses.sort(sortCoursesBySeqNo));
     } catch (err) {
       console.log(err);
